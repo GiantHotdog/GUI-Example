b08d5554c9387ff385e5ca912fd241ab8e8a0bb3
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2024-01-20 13:00:19 +0000

    removed unnecessary comments

--- Example.py
+++ Example.py
@@ -7,17 +7,10 @@ def on_button_press():
     print(entry.get())
 
 
-# def destroy():
-#     window.destroy()
-
-
 window = Tk.Tk()
-# window, text="Press", command=on_button_press)
+
 button = Tk.Button(text="Enter", command=on_button_press)
 button.pack()
-
-# button2 = Tk.Button(window, text="Close", command=destroy)
-# button2.pack()
 
 entry = Tk.Entry(window)
 entry.pack()
