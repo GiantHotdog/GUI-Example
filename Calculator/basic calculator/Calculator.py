a8b977df780b7409e2d43602ae1f0ded224b88d8
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2024-01-24 18:24:15 +0000

    added validation to input

--- Calculator/basic calculator/Calculator.py
+++ Calculator/basic calculator/Calculator.py
@@ -1,10 +1,16 @@
 import tkinter as Tk
+import re
+from tkinter import messagebox
 
 
 def enter_press():
     contents = entry.get()
-    entry.delete(0, Tk.END)
-    entry.insert(0, eval(contents))
+    if re.search("^[0-9\+\-\*/]", contents):
+        entry.delete(0, Tk.END)
+        entry.insert(0, eval(contents))
+    else:
+        messagebox.showerror(
+            "Error.", "contents of input box were not numbers or calculation symbols")
 
 
 window = Tk.Tk()
