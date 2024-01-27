3d1f30f625361894fe06c7eb1e1beb9b25b92118
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2024-01-27 10:36:57 +0000

    finished calculator.py

--- Calculator/basic calculator/Calculator.py
+++ Calculator/basic calculator/Calculator.py
@@ -5,17 +5,18 @@ from tkinter import messagebox
 
 def enter_press():
     contents = entry.get()
-    if re.search("^[0-9\+\-\*/]", contents):
+    if re.search("^[0-9\+\-\*/\(\)]*$", contents):
         entry.delete(0, Tk.END)
         entry.insert(0, eval(contents))
     else:
         messagebox.showerror(
-            "Error.", "contents of input box were not numbers or calculation symbols")
+            "Error", "contents of input box were not numbers or calculation symbols.")
 
 
 window = Tk.Tk()
 window.title("Calculator")
 window.resizable(False, False)
+window.geometry("300x100")
 
 entry = Tk.Entry(window)
 entry.pack()
