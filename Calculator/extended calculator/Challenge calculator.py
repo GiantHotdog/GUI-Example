4796feed9276caca3073df09284e07f200ea0d8f
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2024-01-27 13:29:15 +0000

    removed unneccesary functions

--- Calculator/extended calculator/Challenge calculator.py
+++ Calculator/extended calculator/Challenge calculator.py
@@ -5,100 +5,22 @@ from tkinter import messagebox
 
 def enter_press():
     contents = entry.get()
-    print("validating")
     if re.search("^[0-9\+\-\*/\(\)]*$", contents):
         entry.config(state="normal")
         entry.delete(0, Tk.END)
         entry.insert(0, eval(contents))
         entry.config(state="readonly")
-        print("validates")
     else:
         messagebox.showerror(
             "Error", "contents of input box were not numbers or calculation symbols.")
 
 
-def one():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "1")
-    entry.config(state="readonly")
-
-
-def two():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "2")
-    entry.config(state="readonly")
-
-
-def three():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "3")
-    entry.config(state="readonly")
-
-
-def four():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "4")
-    entry.config(state="readonly")
-
-
-def five():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "5")
-    entry.config(state="readonly")
-
-
-def six():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "6")
-    entry.config(state="readonly")
-
-
-def seven():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "7")
-    entry.config(state="readonly")
-
-
-def eight():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "8")
-    entry.config(state="readonly")
-
-
-def nine():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "9")
-    entry.config(state="readonly")
-
-
-def zero():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "0")
-    entry.config(state="readonly")
-
-
-def plus():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "+")
-    entry.config(state="readonly")
-
-
-def minus():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "-")
-    entry.config(state="readonly")
-
-
-def times():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "*")
-    entry.config(state="readonly")
-
-
-def divide():
-    entry.config(state="normal")
-    entry.insert(Tk.END, "/")
-    entry.config(state="readonly")
+def make_cmd(cmd):
+    def action():
+        entry.config(state="normal")
+        entry.insert(Tk.END, cmd)
+        entry.config(state="readonly")
+    return action
 
 
 def backspace():
@@ -117,53 +39,53 @@ window.geometry("300x500")
 vertical_offset = 50
 horizontal_offset = 15
 
-entry = Tk.Entry(window, foreground="black", )
+entry = Tk.Entry(window)
 entry.config(state="readonly")
 entry.pack()
 
 enter_button = Tk.Button(window, text="Enter", command=enter_press)
 enter_button.place(x=300/2-horizontal_offset, y=vertical_offset+135)
 
-button_one = Tk.Button(window, command=one, text="1")
+button_one = Tk.Button(window, command=make_cmd("1"), text="1")
 button_one.place(x=300/2-45-horizontal_offset, y=vertical_offset)
 
-button_two = Tk.Button(window, command=two, text="2")
+button_two = Tk.Button(window, command=make_cmd("2"), text="2")
 button_two.place(x=300/2-horizontal_offset, y=vertical_offset)
 
-button_three = Tk.Button(window, command=three, text="3")
+button_three = Tk.Button(window, command=make_cmd("3"), text="3")
 button_three.place(x=300/2+45-horizontal_offset, y=vertical_offset)
 
-button_four = Tk.Button(window, command=four, text="4")
+button_four = Tk.Button(window, command=make_cmd("4"), text="4")
 button_four.place(x=300/2-45-horizontal_offset, y=vertical_offset+45)
 
-button_five = Tk.Button(window, command=five, text="5")
+button_five = Tk.Button(window, command=make_cmd("5"), text="5")
 button_five.place(x=300/2-horizontal_offset, y=vertical_offset+45)
 
-button_six = Tk.Button(window, command=six, text="6")
+button_six = Tk.Button(window, command=make_cmd("6"), text="6")
 button_six.place(x=300/2+45-horizontal_offset, y=vertical_offset+45)
 
-button_seven = Tk.Button(window, command=seven, text="7")
+button_seven = Tk.Button(window, command=make_cmd("7"), text="7")
 button_seven.place(x=300/2-45-horizontal_offset, y=vertical_offset+90)
 
-button_eight = Tk.Button(window, command=eight, text="8")
+button_eight = Tk.Button(window, command=make_cmd("8"), text="8")
 button_eight.place(x=300/2-horizontal_offset, y=vertical_offset+90)
 
-button_nine = Tk.Button(window, command=nine, text="9")
+button_nine = Tk.Button(window, command=make_cmd("9"), text="9")
 button_nine.place(x=300/2+45-horizontal_offset, y=vertical_offset+90)
 
-button_zero = Tk.Button(window, command=zero, text="0")
+button_zero = Tk.Button(window, command=make_cmd("0"), text="0")
 button_zero.place(x=300/2-45-horizontal_offset, y=vertical_offset+135)
 
-button_plus = Tk.Button(window, command=plus, text="+")
+button_plus = Tk.Button(window, command=make_cmd("+"), text="+")
 button_plus.place(x=300/2-90-horizontal_offset, y=vertical_offset)
 
-button_minus = Tk.Button(window, command=minus, text="-")
+button_minus = Tk.Button(window, command=make_cmd("-"), text="-")
 button_minus.place(x=300/2-90-horizontal_offset, y=vertical_offset+45)
 
-button_times = Tk.Button(window, command=times, text="*")
+button_times = Tk.Button(window, command=make_cmd("*"), text="*")
 button_times.place(x=300/2-90-horizontal_offset, y=vertical_offset+90)
 
-button_divide = Tk.Button(window, command=divide, text="/")
+button_divide = Tk.Button(window, command=make_cmd("/"), text="/")
 button_divide.place(x=300/2-90-horizontal_offset, y=vertical_offset+135)
 
 button_backspace = Tk.Button(window, command=backspace, text="C")
